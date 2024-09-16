77759678286e197752393ed0c40d98013e26df3a
Andre001 <andre.rufino.correia@gmail.com>
2024-09-16 14:51:32 +0100

    Create the the minimum (main menu ui)

--- Assets/Scripts/Main Menu/High Score.cs
+++ Assets/Scripts/Main Menu/High Score.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class HighScore : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI high_score_txt;  // TextMeshProUGUI instead of TextMeshPro
-
+    [SerializeField] TextMeshProUGUI high_score;  // TextMeshProUGUI component to update in the UI
+    private string high_score_txt = "High Score";
+    
     private void Update()
     {
         int highScore = PlayerPrefs.GetInt("HighScore", 0);
-        high_score_txt.text += highScore.ToString();  // Convert int to string
+        high_score_txt = "High Score: " + highScore.ToString();  // Convert int to string with a label
+        high_score.text = high_score_txt;  // Update the TextMeshProUGUI text property
     }
 }
